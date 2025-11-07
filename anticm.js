ac591a171e60d357113f3f0de84f0939207c4159
Phonglt1210 <3dcheckin@gmail.com>
2025-11-07 19:02:37 +0700

    Create anticm.js

---
+++ anticm.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Khong_hien_thi_sprite
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-15
+// @description  try to take over the world!
+// @author       You
+// @match        https://zigavn.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zigavn.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+       // Modify the function as needed
+   getSpriteFromImage = function(a) {
+	return new BkSprite("")
+   };
+})();
